2d9e331dc8d60348c507117a0c073d35d6fb51c3
Ashutosh Kumar <ashutosh94ya@gmail.com>
2026-01-11 11:38:35 +0530

    mutation and hoisting problems of array , string , objects , function

---
+++ Excersise/exercise1/functionScope&Return.js
@@ -0,0 +1,37 @@
+// ******Function Scope & Return******
+// 
+// 
+//   **********Question1**********
+    // function multiply(a,b){
+    //     return a * b ;
+    //     console.log("After Return") // remaning parts never runs after return
+    // }
+    // let result  = multiply(3,5)
+    // console.log(result) // 15
+// 
+// 
+//    **********Question2**********
+    // function greet(name){
+    //     if(!name){
+    //         return "Hello Guest"
+    //     }
+    //     return "Hello "+ name
+    // }
+    // console.log(greet()) // "Hello guest"
+    // console.log(greet("Ram")) // Hello Ram
+// 
+// 
+// **********Question3**********
+    // let count = 0 
+    // function increament(){
+    //     count++
+    // }
+    // increament()
+    // increament()
+    // console.log(count) //2
+// 
+// 
+// 
+// 
+// 
+// 
--- Excersise/tempCodeRunnerFile.js
+++
@@ -1,2 +0,0 @@
-
-    console.log(person['marks']['science']) // 85
