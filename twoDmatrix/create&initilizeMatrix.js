0b28741559103e9c2258bb05b35fb0ad410c69e4
Ashutosh Kumar <ashutosh94ya@gmail.com>
2026-01-15 13:21:15 +0530

    Addition of Two Matrix

--- twoDmatrix/create&initilizeMatrix.js
+++ twoDmatrix/create&initilizeMatrix.js
@@ -1,6 +1,6 @@
-// Problem 1;  Create and initilize a matrix 
- 
-// input -> row , col , default 
+// Problem 1;  Create and initilize a matrix
+
+// input -> row , col , default
 
 // input ; row = 3 col = 4 default = 1;
 
@@ -11,50 +11,73 @@
 //     [1,1,1,1]
 // ]
 
-
 // **************wayOne(1)*************
 
-function createMatrix(row, col , def ){
-    let mat = []
-    for(let i=0; i<row; i++){
-        let res = []
-        for(let j=0 ; j<col; j++){
-            res.push(def)
-        }
-        mat.push(res)
+function createMatrix(row, col, def) {
+  let mat = [];
+  for (let i = 0; i < row; i++) {
+    let res = [];
+    for (let j = 0; j < col; j++) {
+      res.push(def);
     }
-    return mat
+    mat.push(res);
+  }
+  return mat;
 }
+let row = 3;
+let col = 4;
+let def = 1;
 
+console.log(createMatrix(row, col, def));
 
 // ******************wayTwo(2)****************
 
-function createMatrixTwo(row,col,def){
-    let matrix = []
-    for(let i=0; i<row; i++){
-        matrix[i] = []
-        for(let j=0 ; j<col; j++){
-            matrix[i][j] = def
-        }
+function createMatrixTwo(row, col, def) {
+  let matrix = [];
+  for (let i = 0; i < row; i++) {
+    matrix[i] = [];
+    for (let j = 0; j < col; j++) {
+      matrix[i][j] = def;
     }
-    return matrix
+  }
+  return matrix;
 }
+
+console.log(createMatrixTwo(3, 5, 10));
 
 // ************way3***********
 
-
 // using Array() with fill and map
-function fillMap(row,col,def){
-    return Array(row).fill().map(()=>Array(col).fill(def))
+function fillMap(row, col, def) {
+  return Array(row)
+    .fill()
+    .map(() => Array(col).fill(def));
 }
 
+console.log(fillMap(3, 3, 4));
 
 // ************way3 finished ***********
 
-let row = 3
-let col = 4
-let def = 1
+// ************way4***********
 
-console.log(createMatrix(row,col,def))
-console.log(createMatrixTwo(3,5,10))
-console.log(fillMap(3,3,4))
+// using from method
+
+function fromMethod(row, col, def) {
+  return Array.from(
+    {
+      length: row,
+    },
+    () => {
+      return Array.from(
+        {
+          length: col,
+        },
+        () => def
+      );
+    }
+  );
+}
+
+console.log(fromMethod(3, 3, 9));
+
+// ************way4 finished ***********
---
+++ twoDmatrix/towMatrixSum.js
@@ -0,0 +1,24 @@
+// problem 2 : matrix Addition 
+
+let matrix1 = [[1,2], [3,4]]
+let matrix2 = [[5,6], [7,8]]
+
+// output = [[6,8] , [10,12]]
+
+function matrixAddition(mat1,mat2){
+    let matAdd = []
+    if(mat1.length === mat2.length){
+        for(let i=0; i<mat1.length; i++){
+            let res = []
+            for(let j=0; j<mat2.length;j++){
+                res.push(mat1[i][j] + mat2[i][j])
+            }
+            matAdd.push(res)
+        }
+    }else{
+        return "Diamentions not equal!"
+    }
+    return matAdd
+}
+
+console.log(matrixAddition(matrix1,matrix2))
