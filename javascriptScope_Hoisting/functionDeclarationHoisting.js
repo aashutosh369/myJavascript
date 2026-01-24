9517ca757db3073d4b51880276aa8f2c3e39b224
Ashutosh Kumar <ashutosh94ya@gmail.com>
2026-01-24 17:11:21 +0530

    Function Decleration & Hoisting

---
+++ javascriptScope_Hoisting/functionDeclarationHoisting.js
@@ -0,0 +1,14 @@
+// ############################################################# //
+// Question: 
+greet()
+function greet(){
+    console.log('Hello!')
+}
+// During Memory creation phase  
+// ..>>JS first scan the code  
+// ..>>JS store the whole function in the memory
+// 
+// During executio of code 
+// {greet()}..>> see a function call and search it in memory and execute it 
+
+// ############################################################# //
