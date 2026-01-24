8fc714e3b85deff63dddda2c18efd125f2462394
Ashutosh Kumar <ashutosh94ya@gmail.com>
2026-01-24 16:21:14 +0530

    Updated Function Scope

--- javascriptScope&Hoisting/functionScopeWithVar.js
+++ javascriptScope&Hoisting/functionScopeWithVar.js
@@ -5,3 +5,7 @@ function test(){
     var a = 20;
 }
 console.log(a) // ReferenceError : a is not defined
+
+// ⚡ beacuse: 
+//         var a =20 is inside the function scope not in block scope. 
+//         var respect the function scope and can't be accessible outside the function so it gives- ReferenceError : a is not defined
